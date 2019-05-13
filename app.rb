69343c6a1ccb7d6b371d0137942e8d2ba60151e4
Ella <michaella.latkovic@gmail.com>
2019-05-13 16:20:48 -0700

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello World"
+  end
+
+  get '/name' do
+  "My name is Ella"
+  end
+
+  get '/hometown' do
+  "My hometown is Seattle"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Winter 1"
+  end
+
 end
